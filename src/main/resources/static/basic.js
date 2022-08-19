974ebc36861459fe640bf8201ed8e9003762007b
sejin\admin <seijin0722@naver.com>
2022-08-19 11:13:18 +0900

    4-13 관심상품보여주기 (삼항연잔자사용 : lprice랑 myprice비교 / Product만들때 myprice를 0으로 초기화해서 작동됨)

--- src/main/resources/static/basic.js
+++ src/main/resources/static/basic.js
@@ -51,7 +51,7 @@ function execSearch() {
     // 1. 검색창의 입력값을 가져온다.
     let query = $('#query').val();
     // 2. 검색창 입력값을 검사하고, 입력하지 않았을 경우 focus.
-    if ( query == ''){
+    if (query == '') {
         alert("검색어를 입력해주세요.")
         $('#query').focus();
     }
@@ -59,8 +59,8 @@ function execSearch() {
     $.ajax({
         type: 'GET',
         url: `/api/search?query=${query}`,
-        success: function (response){
-            for (let i = 0; i <response.length ; i++) {
+        success: function (response) {
+            for (let i = 0; i < response.length; i++) {
                 let itemDto = response[i];
                 let tempHtml = addHTML(itemDto);
                 $('#search-result-box').append(tempHtml);
@@ -124,13 +124,41 @@ function showProduct() {
      * 관심상품 HTML 만드는 함수: addProductItem
      */
     // 1. GET /api/products 요청
-    // 2. 관심상품 목록, 검색결과 목록 비우기
-    // 3. for 문마다 관심 상품 HTML 만들어서 관심상품 목록에 붙이기!
+    $.ajax({
+        type: 'GET',
+        url: '/api/products',
+        success: function (response) {
+            // 2. 관심상품 목록, 검색결과 목록 비우기
+            $('#product-container').empty();
+            $('#search-result-box').empty();
+            // 3. for 문마다 관심 상품 HTML 만들어서 관심상품 목록에 붙이기!
+            for (let i = 0; i < response.length; i++) {
+                let product = response[i];
+                let tempHtml = addProductItem(product);
+                $('#product-container').append(tempHtml);
+            }
+        }
+    })
 }
 
 function addProductItem(product) {
     // link, image, title, lprice, myprice 변수 활용하기
-    return ``;
+    return `<div class="product-card" onclick="window.location.href='${product.link}'">
+            <div class="card-header">
+                <img src="${product.image}"
+                     alt="">
+            </div>
+            <div class="card-body">
+                <div class="title">
+                    ${product.title}
+                </div>
+                <div class="lprice">
+                    <span>${numberWithCommas(product.lprice)}</span>원
+                </div>
+                <div class="isgood ${product.lprice <= product.myprice ? '' : 'none'}">
+                </div>
+            </div>
+        </div>`;
 }
 
 function setMyprice() {
